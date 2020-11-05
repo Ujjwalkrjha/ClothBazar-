630d034a9e14e698ec66b19e4136a0c05bab59be
Ujjwal kr jha <ujjwal.kr.jha@outlook.com>
2020-11-05 17:06:16 +0530

    Add project files.

---
+++ ClothBaza.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBaza.Web.Startup))]
+namespace ClothBaza.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
