b000182b0fef0650597fccaff80103e84a88f8e5
Ujjwal kr jha <ujjwal.kr.jha@outlook.com>
2020-12-04 15:58:54 +0530

    crud ops

---
+++ ClothBaza.Web/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothBazar.Services;
+using ClothBazar.Entities;
+
+namespace ClothBaza.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ProductsService productservices = new ProductsService();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productservices.SaveProducts(product);
+            //return View();
+            return RedirectToAction("Index");
+        }
+        public ActionResult ProductTable(string search)
+        {
+            var products = productservices.GetProducts();
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            return PartialView(products);
+        }
+    }
+}
---
+++ ClothBazar.Services/ProductsService.cs
@@ -0,0 +1,57 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+   public class ProductsService
+    {
+        public void SaveProducts(Product products)
+        {
+            using (var context = new CBContext())
+            {
+                context.Products.Add(products);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.ToList();
+            }
+           
+        }
+        public Product GetProduct(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Products.Find(ID);
+            }
+
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;    
+                context.SaveChanges();
+            }
+        }
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                //context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
